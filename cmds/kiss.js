1a956e0e88c0386c94c5c6be07cc685f17a0c092
Qwedi <72884039+Qwedi@users.noreply.github.com>
2020-11-29 10:27:35 +0300

    Bot

---
+++ cmds/kiss.js
@@ -0,0 +1,8 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+module.exports.run = async (bot,message,args) => {
+    message.channel.send('https://pa1.narvii.com/6703/56deba239ddba81953c40a8c31dcb6c82cada176_hq.gif');
+};
+module.exports.help = {
+    name: "kiss"
+};
